c117f3b32974bb79de495c685a804dd508b85ceb
SunQing975 <qing.sun.sq0975@team.vdx.com>
2023-06-13 15:04:54 +0800

    add hot-100 049

---
+++ src/main/java/org/job/hot100/Solution049.java
@@ -0,0 +1,75 @@
+package org.job.hot100;
+
+import java.security.PublicKey;
+import java.util.*;
+
+public class Solution049 {
+
+    public List<List<String>> groupAnagrams2(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray);
+            String s = new String(charArray);
+            if (map.containsKey(s)) {
+                List<String> list = map.get(s);
+                list.add(str);
+                map.put(s, list);
+            } else {
+                List<String> list = new ArrayList<>();
+                list.add(str);
+                map.put(s, list);
+            }
+        }
+        List<List<String>> lists = new ArrayList<>();
+        for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+            lists.add(entry.getValue());
+        }
+        return lists;
+    }
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> lists = new ArrayList<>();
+        List<String> cpList = new ArrayList<>(Arrays.asList(strs));
+        for (int i = 0; i < cpList.size(); i++) {
+            StringBuilder builder = new StringBuilder();
+            char[] charArray = cpList.get(i).toCharArray();
+            List<Character> sortList = new ArrayList<>();
+            for (char c : charArray) {
+                sortList.add(c);
+            }
+            List<Character> list = sortList.stream().sorted(Character::compareTo).toList();
+            for (Character character : list) {
+                builder.append(character);
+            }
+            cpList.set(i, builder.toString());
+        }
+        Map<String, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < cpList.size(); i++) {
+            List<Integer> list = map.get(cpList.get(i));
+            if (Objects.isNull(list)) {
+                List<Integer> integers = new ArrayList<>();
+                integers.add(i);
+                map.put(cpList.get(i), integers);
+            } else {
+                list.add(i);
+            }
+        }
+        for (Map.Entry<String, List<Integer>> entry : map.entrySet()) {
+            List<String> list = new ArrayList<>();
+            List<Integer> value = entry.getValue();
+            for (Integer integer : value) {
+                list.add(strs[integer]);
+            }
+            lists.add(list);
+        }
+        return lists;
+    }
+
+    public static void main(String[] args) {
+        Solution049 solution049 = new Solution049();
+        //List<List<String>> lists = solution049.groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"});
+        List<List<String>> lists = solution049.groupAnagrams2(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"});
+        lists.forEach(System.out::println);
+    }
+}
--- src/main/java/org/job/hot100/Solution118.java
+++ src/main/java/org/job/hot100/Solution118.java
@@ -6,9 +6,7 @@ import java.util.List;
 
 public class Solution118 {
     public List<List<Integer>> generate(int numRows) {
-
         List<List<Integer>> lists = new ArrayList<>();
-
         for (int i = 1; i <= numRows; i++) {
             if (i == 1) {
                 lists.add(Collections.singletonList(1));
@@ -16,29 +14,24 @@ public class Solution118 {
             }
             setList(lists, i);
         }
-
         return lists;
     }
 
     private void setList(List<List<Integer>> lists, int i) {
-
         List<Integer> list = lists.get(i - 2);
         List<Integer> nextList = new ArrayList<>(i);
-        for (int i1 = 0; i1 < i; i1++) {
-            nextList.add(0);
-        }
         if (i == 2) {
-            nextList.set(0, 1);
-            nextList.set(1, 1);
+            nextList.add(1);
+            nextList.add(1);
             lists.add(nextList);
             return;
         }
-        for (int i1 = 0; i1 < nextList.size(); i1++) {
-            if (i1 == 0 || i1 == nextList.size() - 1) {
-                nextList.set(i1, 1);
+        for (int i1 = 0; i1 < i; i1++) {
+            if (i1 == 0 || (i1 == i - 1)) {
+                nextList.add(1);
                 continue;
             }
-            nextList.set(i1, list.get(i1 - 1) + list.get(i1));
+            nextList.add(list.get(i1 - 1) + list.get(i1));
         }
         lists.add(nextList);
     }
